50babac7d3c0bbef912a2fd502d123406d022e97
Mohamed Nader Baccari <101141015+mnbaccari@users.noreply.github.com>
2026-02-24 09:11:49 -0500

    Add waiting summary (#4)
    * Add waiting summary
    
    * update readme

--- src/queueGauges.ts
+++ src/queueGauges.ts
@@ -9,6 +9,7 @@ export interface QueueGauges {
   failed: Gauge<LabelsT>;
   waiting: Gauge<LabelsT>;
   completeSummary: Summary<LabelsT>;
+  waitingSummary: Summary<LabelsT>;
 }
 
 export function makeGuages(
@@ -26,6 +27,14 @@ export function makeGuages(
       registers,
       name: `${statPrefix}complete_duration`,
       help: 'Time to complete jobs',
+      labelNames: ['queue', 'prefix'],
+      maxAgeSeconds: 300,
+      ageBuckets: 13,
+    }),
+    waitingSummary: new Summary({
+      registers,
+      name: `${statPrefix}waiting_duration`,
+      help: 'Time jobs spend waiting in the queue before being processed',
       labelNames: ['queue', 'prefix'],
       maxAgeSeconds: 300,
       ageBuckets: 13,
@@ -67,7 +76,9 @@ export async function getJobCompleteStats(
     return;
   }
   const duration = job.finishedOn - job.processedOn!;
+  const waitingDuration = job.processedOn! - job.timestamp!;
   gauges.completeSummary.observe({ prefix, queue: name }, duration);
+  gauges.waitingSummary.observe({ prefix, queue: name }, waitingDuration);
 }
 
 export async function getStats(
